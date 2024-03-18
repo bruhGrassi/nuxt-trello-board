7b0da4f478ff3ed578a6c5422847e7fcc7c9fd61
bruhGrassi <brunagm88@gmail.com>
2024-03-18 19:59:49 -0300

    feat: init style board

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="p-10 bg-teal-400 h-[100vh] overflow-auto">
     <Board />
   </div>
 </template>
--- components/Board.vue
+++ components/Board.vue
@@ -41,8 +41,12 @@ const columns = ref<Column[]>([
 ]);
 </script>
 <template>
-  <div>
-    <div v-for="column in columns" :key="column.id">
+  <div class="flex gap-4 overflow-x-auto items-start">
+    <div
+      v-for="column in columns"
+      :key="column.id"
+      class="column bg-gray-100 p-5 rounded min-w-[250px]"
+    >
       <header>{{ column.title }}</header>
       <p v-for="task in column.tasks">
         {{ task.title }}
